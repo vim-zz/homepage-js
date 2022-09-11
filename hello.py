8849a0d6a636717baedff50180bbf6267e19c346
shaked zohar <shaked.zrihen@gmail.com>
2022-09-11 20:37:25 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,14 +2,13 @@
 import vacations as v
 
 # Initialize the month list
-months = ["January", "February", "March", "April", "May", "June",
-          "July", "August", "September", "October", "November", "December"]
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 # Initial flag variable to print summer vacation one time
 flag = 0
 
 # Iterate the list using for loop
 for month in months:
-    if month == "June" or month == "July":
+    if month == 'June' or month == "July":
         if flag == 0:
             print("Now",v.vacation1)
             flag = 1
